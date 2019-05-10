76559f693515ec5b571392b87ee95c687b299470
Cody Smith <cms287@gmail.com>
2019-05-09 21:42:59 -0700

    basic responsive layout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import '../styles/App.css';
 
 
 export default class App extends React.Component {
@@ -11,7 +13,31 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div>Hello React</div>
+      <div>
+        <Container fluid className="navbar">
+          <Row>
+            <Col md="12" className="storyTeller">
+              StoryTeller
+            </Col>
+          </Row>
+        </Container>
+        <Container className="mainContainer">
+          <Row>
+            <Col md="8" className="mainStoryDisplay">
+              <h2 className="title">Main Story</h2>
+            </Col>
+            <Col md="4" className="myStoryList">
+              <h2 className="title">My Story List</h2>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="12" className="storyLibrary">
+            <h2 className="title">Story Library</h2>
+            </Col>
+          </Row>
+        </Container>
+
+      </div>
     )
   }
 };
