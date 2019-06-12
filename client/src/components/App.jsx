9d62b757662ad2b9977657a57717ccd757049253
Cody Smith <cms287@gmail.com>
2019-06-12 00:35:41 -0700

    Added The Three Little Pigs to storyData.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,42 +2,42 @@ import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import '../styles/App.css';
 
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
-    }
+      mainStory: '',
+      myStoryList: '',
+      currentLibrary: ''
+    };
   }
 
   render() {
     return (
       <div>
-        <Container fluid className="navbar">
+        <Container fluid className='navbar'>
           <Row>
-            <Col md="12" className="storyTeller">
+            <Col md='12' className='storyTeller'>
               StoryTeller
             </Col>
           </Row>
         </Container>
-        <Container className="mainContainer">
+        <Container className='mainContainer'>
           <Row>
-            <Col md="8" className="mainStoryDisplay">
-              <h2 className="title">Main Story</h2>
+            <Col md='8' className='mainStoryDisplay'>
+              <h2 className='title'>Main Story</h2>
             </Col>
-            <Col md="4" className="myStoryList">
-              <h2 className="title">My Story List</h2>
+            <Col md='4' className='myStoryList'>
+              <h2 className='title'>My Stories</h2>
             </Col>
           </Row>
           <Row>
-            <Col md="12" className="storyLibrary">
-            <h2 className="title">Story Library</h2>
+            <Col md='12' className='storyLibrary'>
+              <h2 className='title'>Story Library</h2>
             </Col>
           </Row>
         </Container>
-
       </div>
-    )
+    );
   }
-};
+}
